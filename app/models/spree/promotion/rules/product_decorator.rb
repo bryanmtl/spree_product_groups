278b2265064b90ae3a690d3e2f67ae69f0f2c536
Bryan Mahoney <bryan@goDynamo.com>
2013-11-11 15:30:23 -0500

    Corrects issue when switching between a product group and manual list of products

--- app/models/spree/promotion/rules/product_decorator.rb
+++ app/models/spree/promotion/rules/product_decorator.rb
@@ -4,16 +4,16 @@ if defined?(Spree::Promotion)
       module Rules
         Product.class_eval do
           belongs_to :product_group
-          attr_accessible :products_source, :product_group_id
+          attr_accessible :product_group_id, :products_source
+          attr_accessor :products_source
+          before_save :reset_product_group
 
           def eligible_products
             product_group ? product_group.products : products
           end
 
-          def products_source=(source)
-            if source.to_s == 'manual'
-              self.product_group_id = nil
-            end
+          def reset_product_group
+            update_column(:product_group_id, nil) if products_source == 'manual'
           end
 
         end

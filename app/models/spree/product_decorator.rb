a8e38ac3d6d67807b2fe83c05e314530fd41ebe9
Bryan Mahoney <bry@godynamo.com>
2014-11-13 14:17:01 -0500

    Disables after save callback

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,8 +1,6 @@
 Spree::Product.class_eval do
   has_and_belongs_to_many :product_groups, :join_table => 'spree_product_groups_products'
 
-  after_save :update_memberships if Spree::ProductGroup.table_exists?
-
   def update_memberships
     self.product_groups = Spree::ProductGroup.all.select { |pg| pg.include?(self) }
   end
